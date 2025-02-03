b974dec3306c22293b1a01b882fad326a37bf2b2
Morison <morisonabinsinguza@gmail.com>
2025-02-03 20:23:54 +0300

    First commit

---
+++ AITS/modules/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class Users(models.Model):
+    
+    pass
+
+class Issues(models.Model):
+    pass    
+
+class Departments(models.Model):
+    pass
