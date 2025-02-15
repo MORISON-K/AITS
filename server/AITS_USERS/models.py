97e7268d82a019c308b0810656ae297208571e67
Dorcus-jane <dorcusjane2003@gmail.com>
2025-02-15 17:48:08 +0300

    creating models

--- server/AITS_USERS/models.py
+++ server/AITS_USERS/models.py
@@ -1,2 +1,31 @@
 from django.db import models
 
+# Create your models here.
+class User(models.Model):
+    Student_ID =models.AutoField(primary_key=True) 
+    name       =models.CharField(max_length= 100)
+    email      =models.EmailField()
+    role       =models.CharField(max_length= 100)
+
+class Issues(models.Model):
+    Issue_ID    = models.AutoField(primary_key=True)
+    Category    = models.CharField(max_length= 100)
+    Description = models.TextField()
+    Status      = models.CharField(max_length=100)
+    Assigned_To = models.CharField(max_length=100)
+
+class Departments(models.Model):
+    ID      =  models.AutoField(primary_key=True)
+    Name    =  models.CharField(max_length= 100)
+    Faculty =  models.CharField(max_length=100)
+
+
+    
+    
+
+        
+
+
+
+
+    
