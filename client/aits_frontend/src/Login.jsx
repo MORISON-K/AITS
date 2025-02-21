0c3b87ae422cb29f4379afd953083fc31eca65fe
atreavez <atreavez@gmail.com>
2025-02-22 01:45:45 +0300

    Update Login.jsx

--- client/aits_frontend/src/Login.jsx
+++ client/aits_frontend/src/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Login = () => {
+const Login = ({ handlePageChange }) => {
   return (
-    <div>
+    <div className="login-page">
       <h1>Login</h1>
       <form>
         <label>Email:</label>
@@ -13,6 +13,9 @@ const Login = () => {
         <br />
         <button type="submit">Login</button>
       </form>
+      <button className="back-button" onClick={() => handlePageChange('welcome')}>
+        Back to Home
+      </button>
     </div>
   );
 };
