3f17a9f9cf109775ccdae0b29d5bb4aefcc8711b
atreavez <atreavez@gmail.com>
2025-02-22 13:53:48 +0300

    Update views.py

--- server/AITS_USERS/views.py
+++ server/AITS_USERS/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import User, Department, Issue
-
 from .serializers import UserSerializer, DepartmentSerializer, IssueSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 @api_view(['GET', 'POST'])
 def user(request):
@@ -15,7 +13,6 @@ def user(request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -29,7 +26,6 @@ def department(request):
         departments = Department.objects.all()
         serializer = DepartmentSerializer(departments, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = DepartmentSerializer(data=request.data)
         if serializer.is_valid():
@@ -37,13 +33,12 @@ def department(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])    
+@api_view(['GET', 'POST'])
 def issue(request):
     if request.method == 'GET':
         issues = Issue.objects.all()
         serializer = IssueSerializer(issues, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = IssueSerializer(data=request.data)
         if serializer.is_valid():
@@ -51,6 +46,18 @@ def issue(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        response_data = {
+            'access': token['access'],
+            'refresh': token['refresh'],
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
 
 
 
