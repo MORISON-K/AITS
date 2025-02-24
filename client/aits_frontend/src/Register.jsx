b3feaede43146db3c9e0f60620316dfeaa1a14bf
Morison <morisonabinsinguza@gmail.com>
2025-02-24 23:15:09 +0300

    Adjusting the Login Page

--- client/aits_frontend/src/Register.jsx
+++ client/aits_frontend/src/Register.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 const Register = ({ handlePageChange }) => {
   return (
-    <div className="register-page">
+    <div className="Register-page">
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder='First Name' />
-        <input type="text" placeholder='Second Name' />
+        <input type="text" placeholder='First Name' className='First-Name' />
+        <input type="text" placeholder='Last Name' className='Last-Name' />
         <br />
-        <input type="email" placeholder='Email'/>
+        <input type="email" placeholder='Email' className='Register-input' />
         <br />
-        <input type="text" placeholder='Select Role'/><br />
-        <input type="text" placeholder='Enter Role ID' /><br />
-        <input type="password" placeholder='Password'/>
+        <input type="text" placeholder='Select Role' className='Register-input' /><br />
+        <input type="text" placeholder='Enter Role ID' className='Register-input' /><br />
+        <input type="password" placeholder='Password' className='Register-input' />
         <br />
-        <input type="text" placeholder='Confirm Password' /><br />
-        <button>Submit</button><br />
-        {/* <button type="submit">Register</button> */}
+        <input type="password" placeholder='Confirm Password' className='Register-input' /><br />
+        <button className='Submit-Button'>Submit</button><br />
       </form>
       {/* <button className="back-button" onClick={() => handlePageChange('welcome')}>
         Back to Home
