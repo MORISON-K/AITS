76080189f8b43956029fb118e86d7464ebba2428
atreavez <atreavez@gmail.com>
2025-02-27 00:55:14 +0300

    Update Login.jsx

--- client/aits_frontend/src/Login.jsx
+++ client/aits_frontend/src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from './auth.js';
+import AuthContext from './auth.js';
 
 const Login = ({ handlePageChange }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
