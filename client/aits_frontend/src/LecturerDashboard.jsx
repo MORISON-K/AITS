81f483f5254b701bb2dd64e26ce6ef7c416db933
atreavez <atreavez@gmail.com>
2025-02-27 02:34:46 +0300

    Create LecturerDashboard.jsx

---
+++ client/aits_frontend/src/LecturerDashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LecturerDashboard = () => {
+  return (
+    <div>
+      <h1>Lecturer Dashboard</h1>
+      <ul>
+        <li><a href="#">View new issues</a></li>
+        <li><a href="#">View pending issues</a></li>
+        <li><a href="#">View solved issues</a></li>
+        <li><a href="#">create issues</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default LecturerDashboard;
+
