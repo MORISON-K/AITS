adc22418ad5a06b181f6d85a88b9d4656e3cfc50
atreavez <atreavez@gmail.com>
2025-02-27 01:59:44 +0300

    Update App.jsx

--- client/aits_frontend/src/App.jsx
+++ client/aits_frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider, AuthContext } from './auth';
@@ -36,6 +37,7 @@ function App() {
                     <IssueSubmission_form />
                   ) : null
                 } />
+                <Route path="/login" element={<Login />} />
                 <Route path="/student-dashboard" element={<StudentDashboard />} />
               </Routes>
             </div>
