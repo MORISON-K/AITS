0ed55bac4286b9d52252ba50c457bf24e1778e09
atreavez <atreavez@gmail.com>
2025-02-27 02:28:40 +0300

    Create RegistrarDashboard.jsx

---
+++ client/aits_frontend/src/RegistrarDashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RegistrarDashboard = () => {
+  return (
+    <div>
+      <h1>Registrar Dashboard</h1>
+      <ul>
+        <li><a href="#">Manage Student issues</a></li>
+        <li><a href="#">Create an issue</a></li>
+        <li><a href="#">assign an issue</a></li>
+        <li><a href="#">Generate issue Report</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default RegistrarDashboard;
