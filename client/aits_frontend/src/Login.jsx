807f81310b2d62446281dd40b9faa83300c01aae
atreavez <atreavez@gmail.com>
2025-02-27 00:43:50 +0300

    Update Login.jsx

--- client/aits_frontend/src/Login.jsx
+++ client/aits_frontend/src/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import { AuthContext } from './auth';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './auth.js';
 
