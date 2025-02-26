788a4e642a54f84ab84c07b8964d7674654eb965
atreavez <atreavez@gmail.com>
2025-02-27 01:42:55 +0300

    Update App.jsx

--- client/aits_frontend/src/App.jsx
+++ client/aits_frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider, AuthContext } from './auth';
 import Login from './Login';
 import Register from './Register';
 import './App.css';
 import IssueSubmission_form from './IssueSubmission_form';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import StudentDashboard from './StudentDashboard';
-import { AuthProvider } from './auth';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <div className="app-container">
-          <nav className="nav-bar">
-            <button className="nav-button" onClick={() => window.location.href = '/'}> Home </button>
-          </nav>
-          <Routes>
-            <Route path="/" element={
-              <AuthContext.Consumer>
-                {({ currentPage, handlePageChange }) => (
+        <AuthContext.Consumer>
+          {({ currentPage, handlePageChange }) => (
+            <div className="app-container">
+              <nav className="nav-bar">
+                <button className="nav-button" onClick={() => handlePageChange('welcome')}> Home </button>
+              </nav>
+              <Routes>
+                <Route path="/" element={
                   currentPage === 'welcome' ? (
                     <div className="welcome-page">
                       <img src="/muk_logo.jpeg" alt="Logo not found" />
@@ -26,7 +26,6 @@ function App() {
                       <div className="button-container">
                         <button className="register-button" onClick={() => handlePageChange('register')}> Register </button>
                         <button className="login-button" onClick={() => handlePageChange('login')}> Login </button>
-                        {/* <button className='issue-button' onClick={() => handlePageChange('issueSubmission')}> Create New Issue </button> */}
                       </div>
                     </div>
                   ) : currentPage === 'login' ? (
@@ -36,12 +35,12 @@ function App() {
                   ) : currentPage === 'issueSubmission' ? (
                     <IssueSubmission_form />
                   ) : null
-                )}
-              </AuthContext.Consumer>
-            } />
-            <Route path="/student-dashboard" element={<StudentDashboard />} />
-          </Routes>
-        </div>
+                } />
+                <Route path="/student-dashboard" element={<StudentDashboard />} />
+              </Routes>
+            </div>
+          )}
+        </AuthContext.Consumer>
       </BrowserRouter>
     </AuthProvider>
   );
