810525f286ed9260b696244c7aea5b7959debee5
Morison <morisonabinsinguza@gmail.com>
2025-03-10 23:41:21 +0300

    Viewing Assigned Isssues

---
+++ client/aits_frontend/src/AssignedIssues.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+const AssignedIssues = () => {
+  // Data to be fetched from an API
+  const data = [
+    { ID: "001", course: "CSC 1101", studentNumber: "24007", category: "Missing Marks", date: "01-10-2021" },
+    { ID: "002", course: "CSC 1101", studentNumber: "24008", category: "Missing Marks", date: "01-10-2021" },
+    { ID: "003", course: "CSC 1100", studentNumber: "24009", category: "Wrong Credentials", date: "01-10-2021" },
+  ];
+
+  return (
+    <div className="assigned-issues">
+      <h1>Assigned Issues</h1>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Course Unit</th>
+              <th scope="col">Student Number</th>
+              <th scope="col">Issue Category</th>
+              <th scope="col">Date Created</th>
+             
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.ID}</td>
+                <td>{row.course}</td>
+                <td>{row.studentNumber}</td>
+                <td>{row.category}</td>
+                <td>{row.date}</td>
+                
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default AssignedIssues;
