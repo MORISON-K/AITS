9988786bca71b17361af412d1ce24a3de10dee00
Morison <morisonabinsinguza@gmail.com>
2025-03-25 23:53:02 +0300

    Updating urls of the main project

--- server/aits/urls.py
+++ server/aits/urls.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from AITS_USERS.views import user, department, issue, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', user),
-    path('department/', department),
-    path('issue/', issue),
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/user/register/', include('AITS_USERS.urls')),  # Registration and other auth endpoints are included here.
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('AITS_USERS.urls')),
 ]
