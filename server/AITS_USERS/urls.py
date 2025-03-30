3c19f6410816ffca7161c67370db0953979b73d8
Morison <morisonabinsinguza@gmail.com>
2025-03-30 11:42:25 +0300

    Updating urls to include routers for colleges, departments and programmes

--- server/AITS_USERS/urls.py
+++ server/AITS_USERS/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
 router = DefaultRouter()
-# Add viewsets to router here if needed
+router.register(r'colleges', views.CollegeViewSet)
+router.register(r'departments', views.DepartmentViewSet)
+router.register(r'programmes', views.ProgrammeViewSet)
+
 
 urlpatterns = [
     path('auth/register/', views.RegisterView.as_view(), name='register'),
