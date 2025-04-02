8fc6564a54f26280eac8e5409770471b5204d64d
Dorcus-jane <dorcusjane2003@gmail.com>
2025-04-02 15:59:17 +0300

    Added send email url

--- server/AITS_USERS/urls.py
+++ server/AITS_USERS/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import SendEmailView
+
 
 
 router = DefaultRouter()
@@ -15,4 +17,5 @@ urlpatterns = [
     path('auth/logout/', views.LogoutView.as_view(), name='logout'),
     path('auth/user/', views.UserDetailView.as_view(), name='user_details'),
     path('', include(router.urls)),
+    path('send-email/', SendEmailView.as_view(), name='send-email'),
 ]
