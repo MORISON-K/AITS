fb100eee0248407c908393a2d98cf6438acfd78e
AmanyireCI <aanyirecindy@gmail.com>
2025-04-07 16:50:29 +0300

    Updates

--- server/AITS_USERS/views.py
+++ server/AITS_USERS/views.py
@@ -167,3 +167,5 @@ class IssueView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+
