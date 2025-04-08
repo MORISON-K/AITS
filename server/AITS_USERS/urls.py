425d0de4f67de7d3100cfabc8d01fd485821315f
Dorcus-jane <dorcusjane2003@gmail.com>
2025-04-08 23:54:24 +0300

    Update urls.py

--- server/AITS_USERS/urls.py
+++ server/AITS_USERS/urls.py
@@ -14,7 +14,7 @@ router.register(r'programmes', views.ProgrammeViewSet) # /programmes/
 # Main URL patterns for the app
 urlpatterns = [
      # User authentication routes
-    path('auth/register/', views.RegisterView.as_view(), name='register'),
+    path('auth/register/', views.RegisterView.as_view(), name='register'), # Register new user
     path('auth/login/', views.CustomTokenObtainPairView.as_view(), name='login'),
     path('auth/logout/', views.LogoutView.as_view(), name='logout'),
     path('auth/user/', views.UserDetailView.as_view(), name='user_details'),
