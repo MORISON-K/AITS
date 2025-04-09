72b70c081eb0153dd346b7826ce2b4bf521e12a6
Morison <morisonabinsinguza@gmail.com>
2025-04-09 12:32:35 +0300

    Updating app urls

--- server/AITS_USERS/urls.py
+++ server/AITS_USERS/urls.py
@@ -18,4 +18,9 @@ urlpatterns = [
     path('auth/user/', views.UserDetailView.as_view(), name='user_details'),
     path('', include(router.urls)),
     path('send-email/', SendEmailView.as_view(), name='send-email'),
+    path('years/', views.YearOptionsView.as_view(), name='year-options'),
+    path('semesters/', views.SemesterOptionsView.as_view(), name='semester-options'),  
+    path('courses/', views.CourseListView.as_view(), name='courses-list'),  
+    path('departments/filtered/', views.DepartmentListView.as_view(), name='department-filtered'),
+    path('programmes/filtered/', views.ProgrammeListView.as_view(), name='programme-filtered'),
 ]
