a8325b81b271971f23aff3abbbe31ec1dab7cb96
Morison <morisonabinsinguza@gmail.com>
2025-04-18 15:45:05 +0300

    Including Assigned issue Component for the lecturer

--- client/aits_frontend/src/LecturerDashboard.jsx
+++ client/aits_frontend/src/LecturerDashboard.jsx
@@ -37,6 +37,10 @@ const Sidebar = ({ handleLogout, user }) => {
           </Link>
         </li>
         <li>
+        <Link to="/AssignedIssues">
+        <i className="bx bxs-folder-open text-xl mr-3"></i>
+            <span className="text">View and Resolve Issues</span>
+          </Link>
           <Link to="/notifications">
             <i className="bx bxs-bell"></i>
             <span className="text">Notifications</span>
