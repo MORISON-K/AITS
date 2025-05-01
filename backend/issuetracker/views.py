ab0347f5e7932bf3964f8873b0e98342214ca76c
AmanyireCI <amanyirecindy@gmail.com>
2025-05-01 11:55:12 +0300

    edited views.py

--- backend/issuetracker/views.py
+++ backend/issuetracker/views.py
@@ -1,3 +1,5 @@
+
+
 from rest_framework import viewsets, permissions, status, generics
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.response import Response
