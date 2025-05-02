b37160bd24556bb202c77382894e8a987a619339
Serena-Robinah <serena.robin2323@gmail.com>
2025-05-03 01:38:30 +0300

    u

--- server/aits/urls.py
+++ server/aits/urls.py
@@ -1,11 +1,29 @@
+# from django.contrib import admin
+# from django.urls import path, include, re_path
+# from django.views.generic import TemplateView
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('AITS_USERS.urls')),
+    
+#     # Serve React app - this should be the last URL pattern
+#     re_path(r'^(?!api/|admin/).*$', TemplateView.as_view(template_name='index.html')),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('AITS_USERS.urls')),
     
-    # Serve React app - this should be the last URL pattern
+    # Serve index.html for all other routes
     re_path(r'^(?!api/|admin/).*$', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Add static file serving for development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
