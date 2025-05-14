758786d607a459f1ae9bd58b99d00a3ede65c60d
biar211 <elijahbiar2@gmail.com>
2025-05-14 18:54:37 +0300

    made few changes

--- server/AITS_USERS/admin.py
+++ server/AITS_USERS/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import User, Issue, Department, College, School, Programme, Course
 
 admin.site.register(User)
+
 # admin.site.register(Issue)
+
 admin.site.register(College)
 admin.site.register(School)
 admin.site.register(Department)
