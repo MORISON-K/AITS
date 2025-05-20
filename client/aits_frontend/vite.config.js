a316b2aa7af301222f2ab4d34920415893732eab
Morison <morisonabinsinguza@gmail.com>
2025-05-20 12:38:28 +0300

    Changes

--- client/aits_frontend/vite.config.js
+++ client/aits_frontend/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  base: '/',
-  server: {
-    fs: {
-      strict: false
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/static/';
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      // assetsDir: 'assets', // This is the default and usually fine
+      // manifest: true, // Optional: useful if you were to integrate with django-vite
+    },
+    base: base, // Important: '/' for dev, '/static/' for build
+    server: {
+      fs: {
+        strict: false
+      }
     }
-  }
+  };
 })
