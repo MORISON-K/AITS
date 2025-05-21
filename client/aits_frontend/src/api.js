c995be1efbd848ba2016dba49eb7bfac430c5e11
Morison <morisonabinsinguza@gmail.com>
2025-05-21 13:13:11 +0300

    Api

--- client/aits_frontend/src/api.js
+++ client/aits_frontend/src/api.js
@@ -1,22 +1,48 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
-const BASE = import.meta.env.VITE_API_URL || '';
-const api = axios.create({
+// import axios from "axios"
+// import { ACCESS_TOKEN } from "./constants"
+// const BASE = import.meta.env.VITE_API_URL || '';
+// const api = axios.create({
    
-    baseURL: BASE,
+//     baseURL: BASE,
+// });
+
+// api.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem(ACCESS_TOKEN);
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error)
+//     }
+// );
+
+// export default api;
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api',  // This will use relative URLs that work in both development and production
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
+// Add request interceptor to include auth token
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
+
