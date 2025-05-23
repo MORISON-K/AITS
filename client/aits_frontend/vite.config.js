091ab87ab2b6e1831b92561cd2dd951f2a0fa439
Serena-Robinah <serena.robin2323@gmail.com>
2025-05-23 13:07:37 +0300

    Include latest frontend build

--- client/aits_frontend/vite.config.js
+++ client/aits_frontend/vite.config.js
@@ -1,22 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/static/';
   return {
     plugins: [react()],
+    base: command === 'serve' ? '/' : '/static/', // ✅ This fixes Heroku static paths
     build: {
       outDir: 'dist',
       emptyOutDir: true,
-      // assetsDir: 'assets', // This is the default and usually fine
-      // manifest: true, // Optional: useful if you were to integrate with django-vite
+      manifest: true,
     },
-    base: base, // Important: '/' for dev, '/static/' for build
-    server: {
-      fs: {
-        strict: false
-      }
-    }
   };
-})
+});
